f979fae46717ea953a645c7dc4ea51d8c58a6502
BioCrossCoder <viscaria_dignics@163.com>
2024-04-21 16:59:39 +0800

    [feature] Add GroupBy function

---
+++ itertools/groupby.go
@@ -0,0 +1,34 @@
+package itertools
+
+import (
+	"flex/common"
+	"reflect"
+)
+
+func GroupBy(entry any, by func(any) any) (iterator common.Iterator, err error) {
+	err = common.IsList(entry)
+	if err != nil {
+		return
+	}
+	value := reflect.ValueOf(entry)
+	groupsMap := make(map[any]any)
+	orders := make(map[int]any)
+	count := 0
+	for _, v := range common.CopyList(value, value.Len()) {
+		key := by(v)
+		group, ok := groupsMap[key]
+		if !ok {
+			group = make([]any, 0)
+			orders[count] = key
+			count++
+		}
+		group = append(group.([]any), v)
+		groupsMap[key] = group
+	}
+	groupsList := make([]any, count)
+	for i := 0; i < count; i++ {
+		groupsList[i] = groupsMap[orders[i]]
+	}
+	iterator = NewListIterator(groupsList)
+	return
+}
---
+++ itertools/groupby_test.go
@@ -0,0 +1,33 @@
+package itertools
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGroupBy(t *testing.T) {
+	convey.Convey("group by item", t, func() {
+		entry := [][2]int{{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 6}, {3, 7}}
+		g, err := GroupBy(entry, func(x any) any {
+			return x.([2]int)[0]
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, []any{
+			[]any{[2]int{1, 2}, [2]int{1, 3}},
+			[]any{[2]int{2, 4}, [2]int{2, 5}},
+			[]any{[2]int{3, 6}, [2]int{3, 7}},
+		}, g.Pour())
+	})
+	convey.Convey("group by condition", t, func() {
+		entry := []int{1, 2, 3, 4, 5, 6, 7}
+		g, err := GroupBy(entry, func(x any) any {
+			return x.(int)%2 == 0
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, []any{
+			[]any{1, 3, 5, 7},
+			[]any{2, 4, 6},
+		}, g.Pour())
+	})
+}
