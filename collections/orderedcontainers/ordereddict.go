4d5d837a394f029ddd3d6280fe97f07e1678218f
BioCrossCoder <viscaria_dignics@163.com>
2024-05-03 00:27:38 +0800

    [feature] Add OrderedDict structure

---
+++ collections/orderedcontainers/ordereddict.go
@@ -0,0 +1,128 @@
+package orderedcontainers
+
+import (
+	"flex/collections/dict"
+	"flex/collections/list"
+	"flex/common"
+)
+
+type OrderedDict struct {
+	dict.Dict
+	sequence list.List
+}
+
+func NewOrderedDict(entries ...[2]any) *OrderedDict {
+	elements := dict.FromEntries()
+	sequence := list.Of()
+	for _, entry := range entries {
+		key := entry[0]
+		value := entry[1]
+		if elements.Has(key) {
+			_ = sequence.Remove(key)
+		}
+		_ = elements.Set(key, value)
+		_ = sequence.Push(key)
+	}
+	return &OrderedDict{elements, sequence}
+}
+
+func (d *OrderedDict) Clear() *OrderedDict {
+	_ = d.Dict.Clear()
+	_ = d.sequence.Clear()
+	return d
+}
+
+func (d *OrderedDict) Set(key, value any) *OrderedDict {
+	if !d.Has(key) {
+		_ = d.sequence.Push(key)
+	}
+	_ = d.Dict.Set(key, value)
+	return d
+}
+
+func (d *OrderedDict) Delete(key any) bool {
+	if d.Has(key) {
+		_ = d.Dict.Delete(key)
+		_ = d.sequence.Remove(key)
+		return true
+	}
+	return false
+}
+
+func (d *OrderedDict) Pop(key any, args ...any) (value any, err error) {
+	if d.Has(key) {
+		_ = d.sequence.Remove(key)
+	}
+	return d.Dict.Pop(key, args...)
+}
+
+func (d *OrderedDict) PopItem() (key, value any, err error) {
+	if d.Empty() {
+		err = common.ErrEmptyDict
+		return
+	}
+	key, _ = d.sequence.Tail()
+	value, _ = d.Pop(key)
+	return
+}
+
+func (d *OrderedDict) Update(another OrderedDict) *OrderedDict {
+	for _, key := range another.Keys() {
+		_ = d.Set(key, another.Get(key))
+	}
+	return d
+}
+
+func (d OrderedDict) Keys() list.List {
+	return d.sequence
+}
+
+func (d OrderedDict) Values() list.List {
+	values := make(list.List, d.Size())
+	for i, key := range d.sequence {
+		values[i] = d.Get(key)
+	}
+	return values
+}
+
+func (d OrderedDict) Items() list.List {
+	items := make(list.List, d.Size())
+	for i, key := range d.sequence {
+		items[i] = [2]any{key, d.Get(key)}
+	}
+	return items
+}
+
+func (d OrderedDict) Copy() OrderedDict {
+	return OrderedDict{
+		d.Dict.Copy(),
+		d.sequence.Copy(),
+	}
+}
+
+func (d OrderedDict) Equal(another OrderedDict) bool {
+	if d.Size() != another.Size() {
+		return false
+	}
+	keys1 := d.Keys()
+	keys2 := another.Keys()
+	for i := 0; i < d.Size(); i++ {
+		key1 := keys1[i]
+		key2 := keys2[i]
+		if key1 != key2 {
+			return false
+		}
+		if d.Get(key1) != another.Get(key2) {
+			return false
+		}
+	}
+	return true
+}
+
+func (d OrderedDict) KeyAt(index int) (any, error) {
+	return d.sequence.At(index)
+}
+
+func (d OrderedDict) IndexOf(key any) int{
+    return d.sequence.IndexOf(key)
+}
---
+++ collections/orderedcontainers/ordereddict_test.go
@@ -0,0 +1,79 @@
+package orderedcontainers
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOrderedDict(t *testing.T) {
+	pairs := [][2]any{
+		{"a", 1},
+		{"b", 2},
+		{"c", 3},
+		{"d", 4},
+	}
+	d := NewOrderedDict(pairs...)
+	size := d.Size()
+	convey.Convey("add a key-value pair to dict", t, func() {
+		d2 := d.Copy()
+		assert.Equal(t, d2.Size(), size)
+		assert.False(t, d2.Has("e"))
+		_ = d2.Set("e", 5)
+		assert.True(t, d2.Has("e"))
+		assert.Equal(t, d2.Size(), size+1)
+		assert.Equal(t, d2.Get("e"), 5)
+		assert.Equal(t, d2.IndexOf("e"), size)
+		key, err := d2.KeyAt(size)
+		assert.Nil(t, err)
+		assert.Equal(t, key, "e")
+	})
+	convey.Convey("remove a key from dict", t, func() {
+		d2 := d.Copy()
+		assert.Equal(t, d2.Size(), size)
+		assert.True(t, d2.Has("b"))
+		assert.True(t, d2.Delete("b"))
+		assert.False(t, d2.Has("b"))
+		assert.Equal(t, d2.Size(), size-1)
+	})
+	convey.Convey("pop a key from dict", t, func() {
+		d2 := d.Copy()
+		assert.Equal(t, d2.Size(), size)
+		assert.True(t, d2.Has("a"))
+		expected := d2.Get("a")
+		value, err := d2.Pop("a")
+		assert.Nil(t, err)
+		assert.Equal(t, expected, value)
+		assert.Equal(t, d2.Size(), size-1)
+		assert.False(t, d2.Has("a"))
+	})
+	convey.Convey("pop a key-value pair from dict", t, func() {
+		d2 := d.Copy()
+		assert.Equal(t, d2.Size(), size)
+		expectedKey, err := d2.Keys().Tail()
+		assert.Nil(t, err)
+		expectedValue := d2.Get(expectedKey)
+		key, value, err := d2.PopItem()
+		assert.Nil(t, err)
+		assert.Equal(t, key, expectedKey)
+		assert.Equal(t, value, expectedValue)
+	})
+	convey.Convey("iterate over dict", t, func() {
+		d2 := d.Copy()
+		keys := d2.Keys()
+		values := d2.Values()
+		items := d2.Items()
+		for i := 0; i < d2.Size(); i++ {
+			assert.Equal(t, [2]any{keys[i], values[i]}, items[i])
+		}
+	})
+	convey.Convey("clear and update a dict", t, func() {
+		d2 := d.Copy()
+		assert.True(t, d.Equal(d2))
+		assert.False(t, d2.Empty())
+		_ = d2.Clear()
+		assert.True(t, d2.Empty())
+		_ = d2.Update(*d)
+		assert.True(t, d.Equal(d2))
+	})
+}
