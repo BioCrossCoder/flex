52299c0a9219ef6a226b7af02547e0e093440953
BioCrossCoder <viscaria_dignics@163.com>
2024-05-03 00:21:57 +0800

    [feature] Add OrderedSet structure

---
+++ collections/orderedcontainers/orderedset.go
@@ -0,0 +1,102 @@
+package orderedcontainers
+
+import (
+	"flex/collections/list"
+	"flex/collections/set"
+	"flex/common"
+)
+
+type OrderedSet struct {
+	set.Set
+	sequence list.List
+}
+
+func NewOrderedSet(entries ...any) *OrderedSet {
+	elements := set.Of()
+	sequence := list.Of()
+	for _, entry := range entries {
+		if elements.Has(entry) {
+			continue
+		}
+		_ = elements.Add(entry)
+		_ = sequence.Push(entry)
+	}
+	return &OrderedSet{elements, sequence}
+}
+
+func (s *OrderedSet) Add(element any) *OrderedSet {
+	if !s.Has(element) {
+		_ = s.Set.Add(element)
+		_ = s.sequence.Push(element)
+	}
+	return s
+}
+
+func (s *OrderedSet) Discard(element any) bool {
+	if s.Has(element) {
+		_ = s.Set.Discard(element)
+		_ = s.sequence.Remove(element)
+		return true
+	}
+	return false
+}
+
+func (s *OrderedSet) Clear() *OrderedSet {
+	_ = s.Set.Clear()
+	_ = s.sequence.Clear()
+	return s
+}
+
+func (s *OrderedSet) Update(another OrderedSet) *OrderedSet {
+	for _, element := range another.Elements() {
+		_ = s.Add(element)
+	}
+	return s
+}
+
+func (s *OrderedSet) Pop() (element any, err error) {
+	if s.Empty() {
+		err = common.ErrEmptySet
+		return
+	}
+	element, _ = s.sequence.Pop()
+	_ = s.Set.Discard(element)
+	return
+}
+
+func (s OrderedSet) Elements() list.List {
+	return s.sequence
+}
+
+func (s OrderedSet) Copy() OrderedSet {
+	return OrderedSet{
+		s.Set.Copy(),
+		s.sequence.Copy(),
+	}
+}
+
+func (s OrderedSet) Equal(another OrderedSet) bool {
+	if s.Size() != another.Size() {
+		return false
+	}
+	elements1 := s.Elements()
+	elements2 := another.Elements()
+	for i := 0; i < s.Size(); i++ {
+		if elements1[i] != elements2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (s OrderedSet) At(index int) (any, error) {
+	return s.sequence.At(index)
+}
+
+func (s OrderedSet) IndexOf(element any) int {
+	return s.sequence.IndexOf(element)
+}
+
+func (s OrderedSet) ToList() list.List {
+	return s.sequence.Copy()
+}
