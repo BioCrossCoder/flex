1066e432723dc6a7e6b29d057c33b6cd92862de1
BioCrossCoder <viscaria_dignics@163.com>
2024-05-05 19:19:28 +0800

    [feature] Add Counter[T] structure

---
+++ typed/collections/counter.go
@@ -0,0 +1,185 @@
+package collections
+
+import (
+	"flex/common"
+	"flex/typed/collections/arraylist"
+	"flex/typed/collections/dict"
+	"flex/typed/collections/set"
+	"math"
+)
+
+type Counter[T comparable] struct {
+	records      dict.Dict[T, int]
+	groups       dict.Dict[int, set.Set[T]]
+	defaultCount int
+}
+
+func NewCounter[T comparable](items arraylist.ArrayList[T], defaultCounts ...int) *Counter[T] {
+	records := make(dict.Dict[T, int])
+	for _, item := range items {
+		_ = records.Set(item, records.Get(item, 0)+1)
+	}
+	groups := make(dict.Dict[int, set.Set[T]])
+	for item, count := range records {
+		members := groups.Get(count, set.Set[T]{})
+		_ = members.Add(item)
+		_ = groups.Set(count, members)
+	}
+	defaultCount := 0
+	if len(defaultCounts) > 0 {
+		defaultCount = defaultCounts[0]
+	}
+	return &Counter[T]{
+		records,
+		groups,
+		defaultCount,
+	}
+}
+
+func (c *Counter[T]) Get(item T) int {
+	return c.records.Get(item, c.defaultCount)
+}
+
+func (c *Counter[T]) Set(item T, count int) *Counter[T] {
+	_ = c.records.Set(item, count)
+	members := c.groups.Get(count, set.Set[T]{})
+	_ = c.groups.Set(count, *members.Add(item))
+	return c
+}
+
+func (c *Counter[T]) Increment(item T, counts ...int) *Counter[T] {
+	count := 1
+	if len(counts) > 0 {
+		count = counts[0]
+	}
+	return c.Set(item, c.Get(item)+count)
+}
+
+func (c *Counter[T]) Subtract(item T, counts ...int) *Counter[T] {
+	count := 1
+	if len(counts) > 0 {
+		count = counts[0]
+	}
+	return c.Set(item, c.Get(item)-count)
+}
+
+func (c *Counter[T]) Remove(item T) (exist bool) {
+	exist = c.records.Has(item)
+	if exist {
+		count := c.Get(item)
+		group := c.groups.Get(count)
+		if group.Discard(item) && group.Empty() {
+			_ = c.groups.Delete(count)
+		}
+		_ = c.records.Delete(item)
+	}
+	return
+}
+
+func (c *Counter[T]) SetDefault(count int) *Counter[T] {
+	c.defaultCount = count
+	return c
+}
+
+func (c *Counter[T]) MostCommon() arraylist.ArrayList[T] {
+	maxCount := 0
+	for count := range c.groups {
+		if count > maxCount {
+			maxCount = count
+		}
+	}
+	items := make(arraylist.ArrayList[T], 0)
+	group := c.groups.Get(maxCount, set.Set[T]{})
+	for item := range group {
+		_ = items.Push(item)
+	}
+	return items
+}
+
+func (c *Counter[T]) LeastCommon() arraylist.ArrayList[T] {
+	minCount := math.MaxInt
+	for count := range c.groups {
+		if count < minCount {
+			minCount = count
+		}
+	}
+	items := make(arraylist.ArrayList[T], 0)
+	group := c.groups.Get(minCount, set.Set[T]{})
+	for item := range group {
+		_ = items.Push(item)
+	}
+	return items
+}
+
+func (c *Counter[T]) Total() (total int) {
+	for _, count := range c.records {
+		total += count
+	}
+	return
+}
+
+func (c *Counter[T]) Elements() arraylist.ArrayList[T] {
+	elements := make(arraylist.ArrayList[T], len(c.records))
+	i := 0
+	for k := range c.records {
+		elements[i] = k
+		i++
+	}
+	return elements
+}
+
+func (c *Counter[T]) Reset() *Counter[T] {
+	items := make(set.Set[T])
+	for k := range c.records {
+		_ = c.records.Set(k, c.defaultCount)
+		_ = items.Add(k)
+	}
+	c.groups = dict.Dict[int, set.Set[T]]{
+		c.defaultCount: items,
+	}
+	return c
+}
+
+func (c *Counter[T]) Clear() *Counter[T] {
+	_ = c.records.Clear()
+	_ = c.groups.Clear()
+	return c
+}
+
+func (c Counter[T]) Equal(another Counter[T]) bool {
+	keys1 := set.Of(c.Elements()...)
+	keys2 := set.Of(another.Elements()...)
+	if !keys1.SymmetricDifference(keys2).Empty() {
+		return false
+	}
+	for k, v := range c.records {
+		if !common.Equal(v, another.Get(k)) {
+			return false
+		}
+	}
+	return true
+}
+
+func (c Counter[T]) Copy() Counter[T] {
+	return Counter[T]{
+		c.records.Copy(),
+		c.groups.Copy(),
+		c.defaultCount,
+	}
+}
+
+func MergeCounts[T comparable](counters ...*Counter[T]) *Counter[T] {
+	counter := &Counter[T]{
+		records:      make(dict.Dict[T, int]),
+		groups:       make(dict.Dict[int, set.Set[T]]),
+		defaultCount: 0,
+	}
+	for _, c := range counters {
+		elements := c.Elements()
+		elements.ForEach(func(a T) T {
+			_ = counter.Increment(a, c.Get(a))
+			return a
+		})
+	}
+	return counter
+}
---
+++ typed/collections/counter_test.go
@@ -0,0 +1,81 @@
+package collections
+
+import (
+	"flex/typed/collections/arraylist"
+	"flex/typed/collections/set"
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	l := arraylist.ArrayList[int]{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	convey.Convey("count elements in a list", t, func() {
+		c := NewCounter(l, -1)
+		nums := c.Elements()
+		assert.Equal(t, set.Of(nums...), set.Of(l...))
+		assert.Equal(t, c.Total(), len(l))
+		maxCount := 0
+		minCount := math.MaxInt
+		for _, num := range nums {
+			count := c.Get(num)
+			if count > maxCount {
+				maxCount = count
+			}
+			if count < minCount {
+				minCount = count
+			}
+			assert.Equal(t, count, l.Count(num))
+		}
+		for _, num := range c.MostCommon() {
+			assert.Equal(t, c.Get(num), maxCount)
+		}
+		for _, num := range c.LeastCommon() {
+			assert.Equal(t, c.Get(num), minCount)
+		}
+	})
+	convey.Convey("modify a counter", t, func() {
+		c := NewCounter(l, -1)
+		assert.Equal(t, c.Get(0), -1)
+		_ = c.Set(0, 10)
+		assert.Equal(t, c.Get(0), 10)
+		_ = c.Increment(0, 2)
+		assert.Equal(t, c.Get(0), 12)
+		_ = c.Subtract(0, 1)
+		assert.Equal(t, c.Get(0), 11)
+		assert.True(t, c.Remove(0))
+		assert.Equal(t, c.Get(0), -1)
+		_ = c.SetDefault(6)
+		assert.Equal(t, c.Get(0), 6)
+	})
+	convey.Convey("reset a counter", t, func() {
+		c := NewCounter(l, -1)
+		nums := c.Elements()
+		rest := arraylist.ArrayList[int]{}
+		for i, num := range nums {
+			assert.Equal(t, c.Get(num), l.Count(num))
+			if i%2 == 0 {
+				assert.True(t, c.Remove(num))
+				assert.Equal(t, c.Get(num), -1)
+			} else {
+				_ = rest.Push(num)
+			}
+		}
+		_ = c.Reset()
+		for _, num := range rest {
+			assert.Equal(t, c.Get(num), -1)
+			assert.True(t, c.Remove(num))
+		}
+		assert.Equal(t, len(c.Clear().Elements()), 0)
+	})
+	convey.Convey("merge several counters", t, func() {
+		c1 := NewCounter(l, -1)
+		c2 := c1.Copy()
+		assert.True(t, c1.Equal(c2))
+		mc := MergeCounts(c1, &c2)
+		for _, element := range c2.Elements() {
+			assert.Equal(t, mc.Get(element), c1.Get(element)+c2.Get(element))
+		}
+	})
+}
