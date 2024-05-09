5fc2fa268acd8b8e69b7f57293c7c77eb244e6b0
BioCrossCoder <viscaria_dignics@163.com>
2024-05-09 11:31:27 +0800

    [fix] Correct the logics of Sort[T]

--- typed/functools/sort.go
+++ typed/functools/sort.go
@@ -2,11 +2,25 @@ package functools
 
 import "slices"
 
-func Sort[T any](entry []T, cmps ...func(a, b T) int) {
-	for _, cmp := range cmps {
-		if !slices.IsSortedFunc(entry, cmp) {
-			slices.SortStableFunc(entry, cmp)
+func cmpFunc[T any](cmps ...func(a, b T) int) func(a, b T) int {
+	return func(a, b T) (v int) {
+		for _, cmp := range cmps {
+			v = cmp(a, b)
+			if v != 0 {
+				break
+			}
 		}
+		return
+	}
+}
+
+func Sort[T any](entry []T, cmps ...func(a, b T) int) {
+	if len(cmps) == 0 {
+		return
+	}
+	f := cmpFunc(cmps...)
+	if !slices.IsSortedFunc(entry, f) {
+		slices.SortFunc(entry, f)
 	}
 }
 
@@ -14,4 +28,8 @@ func Sorted[T any](entry []T, cmps ...func(a, b T) int) []T {
 	result := slices.Clone(entry)
 	Sort(result, cmps...)
 	return result
+}
+
+func IsSorted[T any](entry []T, cmps ...func(a, b T) int) bool {
+	return slices.IsSortedFunc(entry, cmpFunc(cmps...))
 }
--- typed/functools/sort_test.go
+++ typed/functools/sort_test.go
@@ -1,18 +1,39 @@
 package functools
 
 import (
+	"flex/typed/collections/arraylist"
 	"flex/typed/collections/sortedcontainers/sortedlist"
 	"github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
 func TestSort(t *testing.T) {
-	convey.Convey("test sort", t, func() {
+	convey.Convey("simple sort", t, func() {
 		a := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
 		b := Sorted(a, sortedlist.AscendOrder)
 		Sort(a, sortedlist.AscendOrder)
 		assert.Equal(t, []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}, a)
 		assert.Equal(t, a, b)
+	})
+	convey.Convey("complex sort", t, func() {
+		arr := make(arraylist.ArrayList[[3]int], 10)
+		for i := 0; i < 10; i++ {
+			for j := 0; j < 3; j++ {
+				arr[i][j] = rand.Intn(10)
+			}
+		}
+		cmps := []func(a, b [3]int) int{
+			func(a, b [3]int) int {
+				return sortedlist.AscendOrder(a[0], b[0])
+			}, func(a, b [3]int) int {
+				return sortedlist.AscendOrder(a[1], b[1])
+			}, func(a, b [3]int) int {
+				return sortedlist.AscendOrder(a[2], b[2])
+			},
+		}
+		Sort(arr, cmps...)
+		assert.True(t, IsSorted(arr, cmps...))
 	})
 }
