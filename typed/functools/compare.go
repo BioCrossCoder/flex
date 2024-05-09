1b2bfd49ed12e2ec88c65ea1ede3f47c88e67868
BioCrossCoder <viscaria_dignics@163.com>
2024-05-09 11:56:06 +0800

    [feature] Add several functions to typed functools

--- typed/functools/compare.go
+++ typed/functools/compare.go
@@ -15,3 +15,50 @@ func Min[T any](cmp func(T, T) int, v ...T) T {
 	}
 	return slices.MinFunc(v, cmp)
 }
+
+func Equals[T any](cmp func(T, T) int, v ...T) bool {
+	l := len(v)
+	if l <= 1 {
+		return true
+	}
+	for i := 1; i < l; i++ {
+		if !Equal(v[i], v[i-1], cmp) {
+			return false
+		}
+	}
+	return true
+}
+
+func Equal[T any](a, b T, cmp func(T, T) int) bool {
+	return cmp(a, b) == 0
+}
+
+func Less[T any](a, b T, cmp func(T, T) int) bool {
+	return cmp(a, b) < 0
+}
+
+func Greater[T any](a, b T, cmp func(T, T) int) bool {
+	return cmp(a, b) > 0
+}
+
+func IsIncreasing[T any](entry []T, cmp func(T, T) int, strict bool) bool {
+	for i := 1; i < len(entry); i++ {
+		if (strict && !Greater(entry[i], entry[i-1], cmp)) || Less(entry[i], entry[i-1], cmp) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsDecreasing[T any](entry []T, cmp func(T, T) int, strict bool) bool {
+	for i := 1; i < len(entry); i++ {
+		if (strict && !Less(entry[i], entry[i-1], cmp)) || Greater(entry[i], entry[i-1], cmp) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsMonotonic[T any](entry []T, cmp func(T, T) int, strict bool) bool {
+	return IsIncreasing(entry, cmp, strict) || IsDecreasing(entry, cmp, strict)
+}
--- typed/functools/compare_test.go
+++ typed/functools/compare_test.go
@@ -15,4 +15,29 @@ func TestCompare(t *testing.T) {
 		assert.Equal(t, 5, Max(sortedlist.AscendOrder, arr...))
 		assert.Equal(t, 5, Min(sortedlist.DescendOrder, arr...))
 	})
+	convey.Convey("compare elements", t, func() {
+		cmp := func(a, b []int) int {
+			return sortedlist.AscendOrder(a[0], b[0])
+		}
+		arr1 := []int{1, 2}
+		arr2 := []int{1, 3}
+		arr3 := []int{1, 2, 3}
+		arr4 := []int{2, 3}
+		assert.True(t, Equals(cmp, arr1, arr2, arr3))
+		assert.False(t, Equals(cmp, arr1, arr4, arr3))
+	})
+	convey.Convey("check monotonicity of array", t, func() {
+		arr := []int{1, 2, 3, 4, 5}
+		assert.True(t, IsMonotonic(arr, sortedlist.AscendOrder, true))
+		assert.True(t, IsMonotonic(arr, sortedlist.AscendOrder, false))
+		arr2 := []int{1, 2, 3, 4, 4}
+		assert.False(t, IsMonotonic(arr2, sortedlist.AscendOrder, true))
+		assert.True(t, IsMonotonic(arr2, sortedlist.AscendOrder, false))
+		arr3 := []int{5, 4, 3, 2, 1}
+		assert.True(t, IsMonotonic(arr3, sortedlist.AscendOrder, true))
+		assert.True(t, IsMonotonic(arr3, sortedlist.AscendOrder, false))
+		arr4 := []int{5, 3, 3, 2, 1}
+		assert.False(t, IsMonotonic(arr4, sortedlist.AscendOrder, true))
+		assert.True(t, IsMonotonic(arr4, sortedlist.AscendOrder, false))
+	})
 }
