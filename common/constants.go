de71a4391f3e7b9a287fbf85a9cb370da6f788e2
BioCrossCoder <viscaria_dignics@163.com>
2024-05-12 20:39:09 +0800

    [chore] Add annotations to common constants and errors; remove redundant errors

--- common/constants.go
+++ common/constants.go
@@ -1,13 +1,19 @@
+// Package common contains common constants, functions and errors used throughout the flex codebase.
 package common
 
 import "reflect"
 
+// ArrayType incluedes array and slice.
 var ArrayType = []reflect.Kind{reflect.Array, reflect.Slice}
 
+// SequenceType includes array, slice and string.
 var SequenceType = append(ArrayType, reflect.String)
 
+// IterableContainers includes array, slice, string and map.
 var IterableContainers = append(SequenceType, reflect.Map)
 
-const hashTableFillFactor = 0.8
+// hashTableLoadFactor is the preseted load factor of the hash table.
+const hashTableLoadFactor = 0.8
 
+// reHashThreshold is the threshold of the load factor to trigger rehashing.
 const reHashThreshold = 6.5
--- common/errors.go
+++ common/errors.go
@@ -1,45 +1,55 @@
+// Package common contains common constants, functions and errors used throughout the flex codebase.
 package common
 
 import "errors"
 
+// ErrNotFunc is an error indicating that the input parameter is not a function as expected.
 var ErrNotFunc = errors.New("the input parameter is not a function")
 
-var ErrIllegalParamCount = errors.New("illegal number of input parameters")
+// ErrUnexpectedParamCount is an error indicating that the number of input parameters is unexpected.
+var ErrUnexpectedParamCount = errors.New("unexpected number of input parameters")
 
-var ErrIllegalReturnCount = errors.New("illegal number of return values")
+// ErrUnexpectedReturnCount is an error indicating that the number of return values is unexpected.
+var ErrUnexpectedReturnCount = errors.New("unexpected number of return values")
 
+// ErrNotIterable is an error indicating that the input parameter is not iterable as expected.
 var ErrNotIterable = errors.New("the input parameter is not iterable")
 
-var ErrNotSeq = errors.New("the input parameter is not a sequence (slice/array/string)")
+// ErrNotSeq is an error indicating that the input parameter is not a slice, array or string as expected.
+var ErrNotSeq = errors.New("the input parameter is not a slice, array or string")
 
-var ErrNotJudgeFunc = errors.New("the input func is not a function for judge")
+// ErrNotJudgeFunc is an error indicating that the input parameter is not a function returning a boolean value as expected.
+var ErrNotJudgeFunc = errors.New("the input parameter is not a function returning a boolean value")
 
-var ErrNoReturn = errors.New("the input function has no return values")
+// ErrInvalidCapacity is an error indicating that the capacity is not a positive integer as expected.
+var ErrInvalidCapacity = errors.New("the capacity must be a positive integer")
 
-var ErrInvalidCapacity = errors.New("the capacity must be an positive integer")
-
-var ErrNotBool = errors.New("the value is not a boolean value")
-
-var ErrNotMap = errors.New("the input parameter is not a map")
-
+// ErrZeroStep is an error indicating that the step is zero while it is expected to be a non-zero integer.
 var ErrZeroStep = errors.New("the step cannot be zero")
 
+// ErrNotList is an error indicating that the input parameter is not a slice or array as expected.
 var ErrNotList = errors.New("the input parameter must be a slice or array")
 
+// ErrInvalidRange is an error indicating that the range is invalid with the step.
 var ErrInvalidRange = errors.New("the range is invalid with the step: [start < end and step < 0] or [start > end and step > 0]")
 
+// ErrOutOfRange is an error indicating that the index is out of range.
 var ErrOutOfRange = errors.New("the index is out of range")
 
+// ErrListLengthMismatch is an error indicating that the length of the input lists are not fully the same as expected.
 var ErrListLengthMismatch = errors.New("the length of the input lists are not equal")
 
+// ErrTooManyArguments is an error indicating that there are more arguments than expected.
 var ErrTooManyArguments = errors.New("too many arguments")
 
+// ErrEmptyList is an error indicating that the input list is empty while it is expected to have at least one element.
 var ErrEmptyList = errors.New("the input list is empty")
 
+// ErrEmptySet is an error indicating that the input set is empty while it is expected to have at least one element.
 var ErrEmptySet = errors.New("the input set is empty")
 
+// ErrEmptyDict is an error indicating that the input dict is empty while it is expected to have at least one key-value pair.
 var ErrEmptyDict = errors.New("the input dict is empty")
 
+// ErrKeyNotFound is an error indicating that the key is not found in the dict as expected.
 var ErrKeyNotFound = errors.New("the key is not found in the dict")
-
-var ErrInvalidType = errors.New("the entry type is not expected")
