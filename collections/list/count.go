b1a33c9045eeead1e9ec82adde3c584ddf72c538
BioCrossCoder <viscaria_dignics@163.com>
2024-05-19 21:45:59 +0800

    [chore] Add annotations to list functions

--- collections/list/count.go
+++ collections/list/count.go
@@ -1,5 +1,7 @@
+// Package list provides functions for working with lists.
 package list
 
+// ParseCount function returns the count of elements based on the provided length and counts
 func ParseCount(length int, counts ...int) int {
 	if len(counts) == 0 {
 		return 1
@@ -10,6 +12,7 @@ func ParseCount(length int, counts ...int) int {
 	return counts[0]
 }
 
+// SearchCount function returns the count of elements for searching based on the provided length and counts
 func SearchCount(length int, counts ...int) int {
 	if len(counts) == 0 || counts[0] <= 0 {
 		return length
--- collections/list/index.go
+++ collections/list/index.go
@@ -2,6 +2,7 @@ package list
 
 import "github.com/biocrosscoder/flex/common"
 
+// SliceIndex returns the valid index within the slice based on the given index, length, and accessibility flag.
 func SliceIndex(index, length int, accessible bool) int {
 	if index < 0 {
 		index += length
@@ -21,6 +22,7 @@ func SliceIndex(index, length int, accessible bool) int {
 	return index
 }
 
+// ParseIndex returns the valid index within the slice based on the given index and length.
 func ParseIndex(index, length int) int {
 	if index < 0 {
 		index += length
@@ -33,6 +35,7 @@ func ParseIndex(index, length int) int {
 	return index
 }
 
+// IsIndexValid checks if the index is valid within the slice based on the given index and length.
 func IsIndexValid(index, length int) (err error) {
 	if index < 0 || index >= length {
 		err = common.ErrOutOfRange
