51d330ead6cf04bf75e9f07663563d1dc58332f1
BioCrossCoder <viscaria_dignics@163.com>
2024-05-23 20:52:01 +0800

    [test] Add examples to typed/collections/orderedcontainers

--- typed/collections/orderedcontainers/orderedchaindict_test.go
+++ typed/collections/orderedcontainers/orderedchaindict_test.go
@@ -90,3 +90,171 @@ func TestOrderedChainDict(t *testing.T) {
 		assert.Equal(t, fmt.Sprint(d2), fmt.Sprint(d3))
 	})
 }
+
+func ExampleOrderedChainDict() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	// Output: map[c:1 a:2 b:3]
+}
+
+func ExampleOrderedChainDict_Set() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	fmt.Println(d)
+	d.Set("c", 3)
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2]
+	// map[c:3 a:2]
+}
+
+func ExampleOrderedChainDict_Delete() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	d.Delete("a")
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 b:3]
+}
+
+func ExampleOrderedChainDict_PopItem() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	key, value, _ := d.PopItem()
+	fmt.Println(key, value)
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// b 3
+	// map[c:1 a:2]
+}
+
+func ExampleOrderedChainDict_Update() {
+	d1 := NewOrderedChainDict[string, int]()
+	d1.Set("c", 1)
+	d1.Set("a", 2)
+	d1.Set("b", 3)
+	d2 := NewOrderedChainDict[string, int]()
+	d2.Set("d", 5)
+	d2.Set("b", 4)
+	d2.Set("e", 6)
+	fmt.Println(d1)
+	d1.Update(*d2)
+	fmt.Println(d1)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 a:2 b:4 d:5 e:6]
+}
+
+func ExampleOrderedChainDict_Keys() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d.Keys())
+	// Output: [c a b]
+}
+
+func ExampleOrderedChainDict_Values() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d.Values())
+	// Output: [1 2 3]
+}
+
+func ExampleOrderedChainDict_Items() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	for _, item := range d.Items() {
+		fmt.Println(*item)
+	}
+	// Output:
+	// {c 1}
+	// {a 2}
+	// {b 3}
+}
+
+func ExampleOrderedChainDict_Copy() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	d2 := d.Copy()
+	fmt.Println(d2)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 a:2 b:3]
+}
+
+func ExampleOrderedChainDict_Equal() {
+	d1 := NewOrderedChainDict[string, int]()
+	d1.Set("c", 1)
+	d1.Set("a", 2)
+	d1.Set("b", 3)
+	d2 := NewOrderedChainDict[string, int]()
+	d2.Set("c", 1)
+	d2.Set("b", 3)
+	d2.Set("a", 2)
+	d3 := NewOrderedChainDict[string, int]()
+	d3.Set("c", 1)
+	d3.Set("a", 2)
+	d3.Set("b", 3)
+	fmt.Println(d1.Equal(*d2))
+	fmt.Println(d1.Equal(*d3))
+	// Output:
+	// false
+	// true
+}
+
+func ExampleOrderedChainDict_KeyAt() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	key, _ := d.KeyAt(1)
+	fmt.Println(key)
+	// Output: a
+}
+
+func ExampleOrderedChainDict_IndexOf() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	index := d.IndexOf("a")
+	fmt.Println(index)
+	// Output: 1
+}
+
+func ExampleOrderedChainDict_MarshalJSON() {
+	d := NewOrderedChainDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	data, _ := json.Marshal(d)
+	fmt.Println(string(data))
+	// Output: [["c",1],["a",2],["b",3]]
+}
+
+func ExampleOrderedChainDict_UnmarshalJSON() {
+	d := NewOrderedChainDict[string, int]()
+	_ = json.Unmarshal([]byte(`[["c",1],["a",2],["b",3]]`), &d)
+	fmt.Println(d)
+	// Output: map[c:1 a:2 b:3]
+}
--- typed/collections/orderedcontainers/ordereddict_test.go
+++ typed/collections/orderedcontainers/ordereddict_test.go
@@ -90,3 +90,171 @@ func TestOrderedDict(t *testing.T) {
 		assert.Equal(t, fmt.Sprint(d2), fmt.Sprint(d3))
 	})
 }
+
+func ExampleOrderedDict() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	// Output: map[c:1 a:2 b:3]
+}
+
+func ExampleOrderedDict_Set() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	fmt.Println(d)
+	d.Set("c", 3)
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2]
+	// map[c:3 a:2]
+}
+
+func ExampleOrderedDict_Delete() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	d.Delete("a")
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 b:3]
+}
+
+func ExampleOrderedDict_PopItem() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	key, value, _ := d.PopItem()
+	fmt.Println(key, value)
+	fmt.Println(d)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// b 3
+	// map[c:1 a:2]
+}
+
+func ExampleOrderedDict_Update() {
+	d1 := NewOrderedDict[string, int]()
+	d1.Set("c", 1)
+	d1.Set("a", 2)
+	d1.Set("b", 3)
+	d2 := NewOrderedDict[string, int]()
+	d2.Set("d", 5)
+	d2.Set("b", 4)
+	d2.Set("e", 6)
+	fmt.Println(d1)
+	d1.Update(*d2)
+	fmt.Println(d1)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 a:2 b:4 d:5 e:6]
+}
+
+func ExampleOrderedDict_Keys() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d.Keys())
+	// Output: [c a b]
+}
+
+func ExampleOrderedDict_Values() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d.Values())
+	// Output: [1 2 3]
+}
+
+func ExampleOrderedDict_Items() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	for _, item := range d.Items() {
+		fmt.Println(*item)
+	}
+	// Output:
+	// {c 1}
+	// {a 2}
+	// {b 3}
+}
+
+func ExampleOrderedDict_Copy() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	fmt.Println(d)
+	d2 := d.Copy()
+	fmt.Println(d2)
+	// Output:
+	// map[c:1 a:2 b:3]
+	// map[c:1 a:2 b:3]
+}
+
+func ExampleOrderedDict_Equal() {
+	d1 := NewOrderedDict[string, int]()
+	d1.Set("c", 1)
+	d1.Set("a", 2)
+	d1.Set("b", 3)
+	d2 := NewOrderedDict[string, int]()
+	d2.Set("c", 1)
+	d2.Set("b", 3)
+	d2.Set("a", 2)
+	d3 := NewOrderedDict[string, int]()
+	d3.Set("c", 1)
+	d3.Set("a", 2)
+	d3.Set("b", 3)
+	fmt.Println(d1.Equal(*d2))
+	fmt.Println(d1.Equal(*d3))
+	// Output:
+	// false
+	// true
+}
+
+func ExampleOrderedDict_KeyAt() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	key, _ := d.KeyAt(1)
+	fmt.Println(key)
+	// Output: a
+}
+
+func ExampleOrderedDict_IndexOf() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	index := d.IndexOf("a")
+	fmt.Println(index)
+	// Output: 1
+}
+
+func ExampleOrderedDict_MarshalJSON() {
+	d := NewOrderedDict[string, int]()
+	d.Set("c", 1)
+	d.Set("a", 2)
+	d.Set("b", 3)
+	data, _ := json.Marshal(d)
+	fmt.Println(string(data))
+	// Output: [["c",1],["a",2],["b",3]]
+}
+
+func ExampleOrderedDict_UnmarshalJSON() {
+	d := NewOrderedDict[string, int]()
+	_ = json.Unmarshal([]byte(`[["c",1],["a",2],["b",3]]`), &d)
+	fmt.Println(d)
+	// Output: map[c:1 a:2 b:3]
+}
